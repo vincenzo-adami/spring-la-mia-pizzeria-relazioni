8ca4f3768548f45924df37cd81a4dde60b684ccd
vincenzo-adami <51445073+vincenzo-adami@users.noreply.github.com>
2024-11-26 18:19:11 +0100

    EDIT: rimossa importazione inutilizzata

--- src/main/java/org/lessons/pizzeria/controller/IngredientsController.java
+++ src/main/java/org/lessons/pizzeria/controller/IngredientsController.java
@@ -15,12 +15,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/ingredients")
